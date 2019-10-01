6e73e401099ea034396bee707beb8a13908d2ad6
rajatkumar-96 <neverbethefollower@gmail.com>
2019-10-01 15:49:47 +0530

    aware interface implemented

--- src/main/java/domain/Movie.java
+++ src/main/java/domain/Movie.java
@@ -1,8 +1,12 @@
 package domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanNameAware {
 
 
     public Movie(Actor actor) {
@@ -10,8 +14,28 @@ public class Movie {
     }
 
     Actor actor;
+    ApplicationContext context=null;
+    Movie ap;
+
+
 
     public void display() {
         System.out.println(actor.name + "  " + actor.gender + " " + actor.age);
     }
+
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println("Bean name is:"+ beanName);
+    }
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.context=context;
+        ap= context.getBean("movie",Movie.class);
+        ap.display();
+
+
+    }
+
 }
